b2ff57d8f686f36ba4443bb01e501d6dec4352d9
António Regadas <apregadas@gmail.com>
2023-01-16 15:14:15 +0000

    chore(types): adds necessary enum that is shared between the project (#41)
    Co-authored-by: Antonio Regadas <antonio.regadas@consensys.net>

---
+++ packages/types/src/enum/MetaMaskTransactionStatuses.ts
@@ -0,0 +1,10 @@
+export enum MetaMaskTransactionStatuses {
+  UNAPPROVED = "unapproved",
+  APPROVED = "approved",
+  REJECTED = "rejected",
+  SIGNED = "signed",
+  SUBMITTED = "submitted",
+  FAILED = "failed",
+  DROPPED = "dropped",
+  CONFIRMED = "confirmed",
+}
