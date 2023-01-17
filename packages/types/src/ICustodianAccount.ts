428a6b8a5aa34accfd080dcbe23eae9cbf7be284
António Regadas <apregadas@gmail.com>
2023-01-17 08:44:02 +0000

    chore(types): adds two interfaces in Types package (#46)
    * chore(types): adds a type from Types
    
    * chore(types): adds to index.ts
    
    * chore(types): adds ICustodianAccount, IExtensionCustodianAccount
    
    Co-authored-by: Antonio Regadas <antonio.regadas@consensys.net>

---
+++ packages/types/src/ICustodianAccount.ts
@@ -0,0 +1,32 @@
+import { AuthDetails } from "./types/AuthDetails";
+
+interface Label {
+  key: string;
+  value: string;
+}
+
+// This is a horrible mess
+
+interface ICustodianAccountProto {
+  name?: string;
+  address: string;
+  custodianDetails: any;
+  labels: Label[];
+  apiUrl: string;
+  chainId?: number;
+  custodyType: string;
+  meta?: { version: number };
+}
+
+// The type actually used in CustodyKeyring
+
+export interface ICustodianAccount<T extends AuthDetails = AuthDetails>
+  extends ICustodianAccountProto {
+  authDetails: T;
+}
+
+// The type that's used in the extension, which is agnostic to authType
+
+export interface IExtensionCustodianAccount extends ICustodianAccountProto {
+  token: string; // TODO
+}
