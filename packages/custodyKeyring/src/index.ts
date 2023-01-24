1972344b52191ed5b39f7657e9f8249f99c936c5
Albert Olivé <albertolivecorbella@gmail.com>
2023-01-24 16:46:03 +0100

    chore(custody-keyring): add jupiterCustodyKeyring export (#67)

--- packages/custodyKeyring/src/index.ts
+++ packages/custodyKeyring/src/index.ts
@@ -1,5 +1,6 @@
 export { CUSTODIAN_TYPES } from "./custodianTypes";
 export { CustodyKeyring } from "./CustodyKeyring";
 export { Migrator } from "./migrations/migrator";
-export { MmiConfigurationController } from "./mmiConfiguration";
 export { MMIConfiguration } from "./types/MMIConfiguration";
+export { MmiConfigurationController } from "./mmiConfiguration";
+export { JupiterCustodyKeyring } from "./custodianTypes/jupiter/JupiterCustodyKeyring";
