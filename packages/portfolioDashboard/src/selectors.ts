7581b67ec3d0355065bcb4e81a54d0ec7b2a9af4
António Regadas <apregadas@gmail.com>
2023-01-31 15:02:09 +0000

    MMI adds portfolio-dashboard package (#78)
    * feat: adds portfolio-dashboard functionality
    
    * fix(portfolio-dashboard): renames package folder to be camelCase and adds jest-fetch-mock
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    ---------
    
    Co-authored-by: Antonio Regadas <antonio.regadas@consensys.net>

---
+++ packages/portfolioDashboard/src/selectors.ts
@@ -0,0 +1,7 @@
+export function getMmiPortfolioEnabled(state): boolean {
+  return state.metamask.mmiConfiguration?.portfolio?.enabled;
+}
+
+export function getMmiPortfolioUrl(state): string {
+  return state.metamask.mmiConfiguration?.portfolio?.url;
+}
