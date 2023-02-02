87c8d644bb6907e86d2fb4cb767f8954b4a16672
Antonio Regadas <antonio.regadas@consensys.net>
2023-02-02 14:44:18 +0000

    fix: clean up coments

--- packages/portfolioDashboard/src/index.ts
+++ packages/portfolioDashboard/src/index.ts
@@ -1,5 +1,4 @@
 export { getMmiPortfolioEnabled, getMmiPortfolioUrl } from "./selectors";
 export { setDashboardCookie } from "./utils";
 
-// Interfaces
 export { ExtensionDashboardResponse } from "./interfaces/ExtensionDashboardResponse";
