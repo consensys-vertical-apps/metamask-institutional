b3c6af2ce209fe250a071eb24bc689f513b9d17e
Albert Olivé <albertolivecorbella@gmail.com>
2023-02-02 11:43:46 +0100

    fix(configurationclient): fixing changelog generator (#92)

--- packages/configurationClient/src/ConfigurationClient.ts
+++ packages/configurationClient/src/ConfigurationClient.ts
@@ -13,7 +13,7 @@ export class ConfigurationClient {
       const configData = await response.json();
       return configData as IConfiguration;
     } catch (e) {
-      console.log(`Error fetching MMI configuration`);
+      console.error(`Error fetching MMI configuration`);
       throw new Error(e);
     }
   }
