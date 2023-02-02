eb050967eff481b5bffe0fa484aeb71e25257dbe
Antonio Regadas <antonio.regadas@consensys.net>
2023-02-02 11:51:14 +0000

    refactor(custodykeyring): exports interface

--- packages/custodyKeyring/src/index.ts
+++ packages/custodyKeyring/src/index.ts
@@ -2,6 +2,7 @@ export { CUSTODIAN_TYPES } from "./custodianTypes";
 export { CustodyKeyring } from "./CustodyKeyring";
 export { Migrator } from "./migrations/migrator";
 export { MMIConfiguration } from "./types/MMIConfiguration";
+export { IJsonRpcCustodian } from "./interfaces/IJsonRpcCustodian";
 export { MmiConfigurationController } from "./mmiConfiguration";
 
 export { IMmiConfigurationControllerOptions } from "./interfaces/IMmiConfigurationControllerOptions";
