72cf02c0130ae20c26c51aa41d92ef8a4137ef49
Antonio Regadas <antonio.regadas@consensys.net>
2023-02-02 12:07:42 +0000

    refactor(custodykeyring): adds the migrator exports to index.ts

--- packages/custodyKeyring/src/index.ts
+++ packages/custodyKeyring/src/index.ts
@@ -1,9 +1,12 @@
 export { CUSTODIAN_TYPES } from "./custodianTypes";
 export { CustodyKeyring } from "./CustodyKeyring";
-export { Migrator } from "./migrations/migrator";
 export { MMIConfiguration } from "./types/MMIConfiguration";
 export { IJsonRpcCustodian } from "./interfaces/IJsonRpcCustodian";
 export { MmiConfigurationController } from "./mmiConfiguration";
 
 export { IMmiConfigurationControllerOptions } from "./interfaces/IMmiConfigurationControllerOptions";
 export { JupiterCustodyKeyring } from "./custodianTypes/jupiter/JupiterCustodyKeyring";
+
+// @TODO Check if we need to export here the Migrator or it will live in it's own package
+export { Migrator } from "./migrations/migrator";
+export { migrations } from "./migrations";
