58a09fc8d4e700ef0b702d37b30294b4f46e9d5b
Antonio Regadas <antonio.regadas@consensys.net>
2023-08-02 14:31:06 +0100

    perf: adds jest rule to each package

--- packages/custodyController/jest.config.js
+++ packages/custodyController/jest.config.js
@@ -1,5 +1,22 @@
 const baseConfig = require("../../jest.config.js");
+const path = require("path");
+const { lstatSync, readdirSync } = require("fs");
+
+// get listing of packages in the mono repo
+const basePath = path.resolve(__dirname);
+const packages = readdirSync(basePath).filter(name => {
+  return lstatSync(path.join(basePath, name)).isDirectory();
+});
 
 module.exports = {
   ...baseConfig,
+  moduleNameMapper: {
+    ...packages.reduce(
+      (acc, name) => ({
+        ...acc,
+        [`@metamask-institutional/${name}(.*)$`]: `<rootDir>/packages/../../${name}/src/$1`,
+      }),
+      {},
+    ),
+  },
 };
