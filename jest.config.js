fb1464a71eb1eb03b0c7be4f2fbd7bc201691302
Antonio Regadas <antonio.regadas@consensys.net>
2023-08-02 14:14:33 +0100

    docs: adds badges to readme file

--- jest.config.js
+++ jest.config.js
@@ -62,7 +62,7 @@ module.exports = {
   // ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ["json", "html"],
+  coverageReporters: ["json", "html", "json-summary", "lcov"],
 
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
