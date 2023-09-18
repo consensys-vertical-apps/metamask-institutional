5e79afa4433cf03aa5c5f00f48fb3d32a579fd62
António Regadas <apregadas@gmail.com>
2023-09-18 12:22:58 +0100

    fix(bump version manually): manually bump version of custody-keyring (#475)

--- packages/custodyKeyring/src/custodianTypes/index.ts
+++ packages/custodyKeyring/src/custodianTypes/index.ts
@@ -8,29 +8,6 @@ import { JupiterCustodyKeyring } from "./jupiter/JupiterCustodyKeyring";
 import { QredoCustodyKeyring } from "./qredo/QredoCustodyKeyring";
 
 export const CUSTODIAN_TYPES: { [key: string]: ICustodianType } = {
-  JUPITER: {
-    name: "Jupiter",
-    displayName: "Jupiter Custody",
-    apiUrl: "https://jupiter-custody.codefi.network",
-    imgSrc: "https://saturn-custody-ui.metamask-institutional.io/saturn.svg",
-    icon: "https://saturn-custody-ui.metamask-institutional.io/saturn.svg",
-    website: '',
-    envName: '',
-    keyringClass: JupiterCustodyKeyring,
-    production: true,
-    hidden: false,
-    origins: [/^https:\/\/jupiter-custody-ui.codefi.network\//],
-    environmentMapping: [
-      {
-        pattern: /^http:\/\/test-pattern/,
-        mmiApiUrl: "http://test-url",
-      },
-      {
-        pattern: /^http:\/\/localhost.*$/,
-        mmiApiUrl: "http://localhost:3000/v1",
-      },
-    ],
-  },
   QREDO: {
     name: "Qredo",
     displayName: "Qredo",
@@ -104,9 +81,31 @@ export const CUSTODIAN_TYPES: { [key: string]: ICustodianType } = {
       },
     ],
   },
+  JUPITER: {
+    name: "Jupiter",
+    displayName: "Jupiter Custody",
+    apiUrl: "https://jupiter-custody.codefi.network",
+    imgSrc: "https://saturn-custody-ui.metamask-institutional.io/saturn.svg",
+    icon: "https://saturn-custody-ui.metamask-institutional.io/saturn.svg",
+    website: '',
+    envName: '',
+    keyringClass: JupiterCustodyKeyring,
+    production: true,
+    hidden: false,
+    origins: [/^https:\/\/jupiter-custody-ui.codefi.network\//],
+    environmentMapping: [
+      {
+        pattern: /^http:\/\/test-pattern/,
+        mmiApiUrl: "http://test-url",
+      },
+      {
+        pattern: /^http:\/\/localhost.*$/,
+        mmiApiUrl: "http://localhost:3000/v1",
+      },
+    ],
+  },
 
   // All new custodians are an instance of this type
-
   JSONRPC: {
     name: "JSONRPC",
     displayName: "JSON-RPC",
@@ -123,7 +122,6 @@ export const CUSTODIAN_TYPES: { [key: string]: ICustodianType } = {
   },
 
   // Legacy Custodian
-
   CURV: {
     name: "Curv",
     displayName: "Curv",
