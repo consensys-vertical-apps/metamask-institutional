5156ca68cec9ef34e2e89c706031cc379c7858e5
Antonio Regadas <antonio.regadas@consensys.net>
2023-11-08 17:06:53 +0000

    fix(response): response result

--- packages/sdk/src/custodianApi/eca3/rpc-responses/JsonRpcGetTransactionByIdResponse.ts
+++ packages/sdk/src/custodianApi/eca3/rpc-responses/JsonRpcGetTransactionByIdResponse.ts
@@ -1,25 +1,27 @@
 export interface JsonRpcGetTransactionByIdResponse {
-  id: string;
-  type: string;
-  from: string;
-  to: string;
-  value: string;
-  gas: string;
-  gasPrice?: string;
-  maxPriorityFeePerGas?: string;
-  maxFeePerGas?: string;
-  nonce: string;
-  data: string;
-  hash: string;
-  status: {
-    finished: boolean;
-    submitted: boolean;
-    signed: boolean;
-    success: boolean;
-    displayText: string;
-    reason: string;
-  };
-  signedRawTransaction?: string;
+  transaction: {
+    id: string;
+    type: string;
+    from: string;
+    to: string;
+    value: string;
+    gas: string;
+    gasPrice?: string;
+    maxPriorityFeePerGas?: string;
+    maxFeePerGas?: string;
+    nonce: string;
+    data: string;
+    hash: string;
+    status: {
+      finished: boolean;
+      submitted: boolean;
+      signed: boolean;
+      success: boolean;
+      displayText: string;
+      reason: string;
+    };
+    signedRawTransaction?: string;
+  },
   metadata?: {
     chainId: number;
     custodianPublishesTransaction: boolean;
