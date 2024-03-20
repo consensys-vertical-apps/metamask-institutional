30f4e6de49ed5813a350fbf904b3cfddb434b694
Albert Olive <albertolivecorbella@gmail.com>
2024-03-20 12:19:50 +0100

    feat(websocketclient transasctionupdate): initial handshake work

--- packages/websocketClient/src/interfaces/IWebsocketClientControllerOptions.ts
+++ packages/websocketClient/src/interfaces/IWebsocketClientControllerOptions.ts
@@ -5,6 +5,7 @@ export class IWebsocketClientControllerOptions {
   initState?: Record<string, unknown>;
   getCustodyKeyring?: (address) => Promise<CustodyKeyring>;
   handleUpdateEvent?: (ev: ICustodianUpdate) => void;
+  handleHandShakeEvent?: (ev: ICustodianUpdate) => void;
   onFailure?: () => void;
   mmiConfigurationController: MmiConfigurationController;
   captureException: (error: Error) => void;
