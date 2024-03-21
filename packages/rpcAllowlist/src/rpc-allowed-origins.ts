943a166aadce044917f58a26cf15f36d6c72b51e
Antonio Regadas <antonio.regadas@consensys.net>
2024-03-21 14:49:11 +0000

    fix(regexes): clean up regexes

--- packages/rpcAllowlist/src/rpc-allowed-origins.ts
+++ packages/rpcAllowlist/src/rpc-allowed-origins.ts
@@ -3,12 +3,12 @@ export const RPC_ALLOWED_ORIGINS = {
     /^http:\/\/localhost:3001/,
     /^http:\/\/host.docker.internal:3001/,
     /^https:\/\/(.*)?metamask-institutional.io/,
-    /^https:\/\/[a-zA-Z0-9-]*.codefi.network/,
+    /^https:\/\/.+\.metamask-institutional.io/,
   ],
   metamaskinstitutional_open_swaps: [
     /^http:\/\/localhost:3001/,
     /^http:\/\/host.docker.internal:3001/,
     /^https:\/\/(.*)?metamask-institutional.io/,
-    /^https:\/\/[a-zA-Z0-9-]*.codefi.network/,
+    /^https:\/\/.+\.metamask-institutional.io/
   ],
 };
