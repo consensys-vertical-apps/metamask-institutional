ed2d7e14cf84cd5a1ca8384d12c8a87e45ce5a59
Antonio Regadas <antonio.regadas@consensys.net>
2024-03-21 14:51:56 +0000

    fix(regex): clean up

--- packages/rpcAllowlist/src/rpc-allowed-origins.ts
+++ packages/rpcAllowlist/src/rpc-allowed-origins.ts
@@ -2,13 +2,11 @@ export const RPC_ALLOWED_ORIGINS = {
   metamaskinstitutional_portfolio: [
     /^http:\/\/localhost:3001/,
     /^http:\/\/host.docker.internal:3001/,
-    /^https:\/\/(.*)?metamask-institutional.io/,
     /^https:\/\/.+\.metamask-institutional.io/,
   ],
   metamaskinstitutional_open_swaps: [
     /^http:\/\/localhost:3001/,
     /^http:\/\/host.docker.internal:3001/,
-    /^https:\/\/(.*)?metamask-institutional.io/,
     /^https:\/\/.+\.metamask-institutional.io/
   ],
 };
