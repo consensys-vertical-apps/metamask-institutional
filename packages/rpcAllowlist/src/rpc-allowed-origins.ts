c66561b745b0055546f14fe0acb652aed8e731ab
António Regadas <apregadas@gmail.com>
2024-03-21 15:04:02 +0000

    Mmi 4835 clean up and fix rpc allowed origins regexes (#673)
    * fix(regexes): clean up regexes
    
    * fix(regex): clean up

--- packages/rpcAllowlist/src/rpc-allowed-origins.ts
+++ packages/rpcAllowlist/src/rpc-allowed-origins.ts
@@ -2,13 +2,11 @@ export const RPC_ALLOWED_ORIGINS = {
   metamaskinstitutional_portfolio: [
     /^http:\/\/localhost:3001/,
     /^http:\/\/host.docker.internal:3001/,
-    /^https:\/\/(.*)?metamask-institutional.io/,
-    /^https:\/\/[a-zA-Z0-9-]*.codefi.network/,
+    /^https:\/\/.+\.metamask-institutional.io/,
   ],
   metamaskinstitutional_open_swaps: [
     /^http:\/\/localhost:3001/,
     /^http:\/\/host.docker.internal:3001/,
-    /^https:\/\/(.*)?metamask-institutional.io/,
-    /^https:\/\/[a-zA-Z0-9-]*.codefi.network/,
+    /^https:\/\/.+\.metamask-institutional.io/
   ],
 };
