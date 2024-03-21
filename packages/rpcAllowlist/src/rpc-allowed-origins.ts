8568b89408f8b12e3a9cedd4f4fb4bb47a930106
António Regadas <apregadas@gmail.com>
2024-03-21 15:12:31 +0000

    fix(lint): lint and prettier fix (#674)

--- packages/rpcAllowlist/src/rpc-allowed-origins.ts
+++ packages/rpcAllowlist/src/rpc-allowed-origins.ts
@@ -7,6 +7,6 @@ export const RPC_ALLOWED_ORIGINS = {
   metamaskinstitutional_open_swaps: [
     /^http:\/\/localhost:3001/,
     /^http:\/\/host.docker.internal:3001/,
-    /^https:\/\/.+\.metamask-institutional.io/
+    /^https:\/\/.+\.metamask-institutional.io/,
   ],
 };
