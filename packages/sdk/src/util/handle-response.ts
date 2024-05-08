fc60c5fdd279a2b27aaeea423b33a85452d2cb52
Albert Olive <albertolivecorbella@gmail.com>
2024-05-08 13:39:36 +0200

    refactor(sdk): improve code

--- packages/sdk/src/util/handle-response.ts
+++ packages/sdk/src/util/handle-response.ts
@@ -1,24 +1,29 @@
 export async function handleResponse(response: Response, contextMessage?: string): Promise<any> {
-  let data: any;
+  if (!response.ok) {
+    try {
+      const errorData = await response.json();
+      let errorMsg = `Error with request. Status: ${response.status} Status text: ${response.statusText}`;
+      if (errorData.error && errorData.error.message) {
+        errorMsg += `. Error message: ${errorData.error.message}`;
+      } else if (errorData.message) {
+        errorMsg += `. Error message: ${errorData.message}`;
+      }
+      errorMsg += contextMessage ? ` Context: ${contextMessage}` : "";
+      console.error(errorMsg, { headers: response.headers, url: response.url });
+      throw new Error(errorMsg);
+    } catch (error) {
+      const fallbackErrorMsg = `Failed to parse JSON from a non-OK response. Status: ${response.status} ${response.statusText}. ${contextMessage || ""}`;
+      console.error(fallbackErrorMsg, { headers: response.headers, url: response.url });
+      throw new Error(fallbackErrorMsg);
+    }
+  }
+
   try {
-    data = await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
     const errorMsg = `Failed to parse JSON. Status: ${response.status} ${response.statusText}. ${contextMessage || ""}`;
     console.error(errorMsg, { headers: response.headers, url: response.url });
     throw new Error(errorMsg);
   }
-
-  if (!response.ok) {
-    let errorMsg = `Error with request. Status: ${response.status} Status text: ${response.statusText}`;
-    if (data.error && data.error.message) {
-      errorMsg += `. Error message: ${data.error.message}`;
-    } else if (data.message) {
-      errorMsg += `. Error message: ${data.message}`;
-    }
-    errorMsg += contextMessage ? ` Context: ${contextMessage}` : "";
-    console.error(errorMsg, { headers: response.headers, url: response.url });
-    throw new Error(errorMsg);
-  }
-
-  return data;
 }
