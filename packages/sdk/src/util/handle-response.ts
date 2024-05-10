b5001c0a26d866f6323998ee3ee2deb5429b5920
Albert Olive <albertolivecorbella@gmail.com>
2024-05-10 10:24:04 +0200

    chore: improve code

--- packages/sdk/src/util/handle-response.ts
+++ packages/sdk/src/util/handle-response.ts
@@ -1,20 +1,16 @@
 export async function handleResponse(response: Response, contextErrorMessage?: string): Promise<any> {
+  let errorMsg = `Error with request. Status: ${response.status} Status text: ${response.statusText}. ${contextErrorMessage}`;
+
   if (!response.ok) {
     try {
       const errorData = await response.json();
-      let errorMsg = `Error with request. Status: ${response.status} Status text: ${response.statusText}`;
-      if (errorData.error && errorData.error.message) {
-        errorMsg += `. Error message: ${errorData.error.message}`;
-      } else if (errorData.message) {
-        errorMsg += `. Error message: ${errorData.message}`;
-      }
-      errorMsg += contextErrorMessage ? ` Context: ${contextErrorMessage}` : "";
+      errorMsg += errorData.error?.message ? `. Error message: ${errorData.error.message}` : `. Error message: ${errorData.message}`;
       console.error(errorMsg, { headers: response.headers, url: response.url });
       throw new Error(errorMsg);
     } catch (error) {
-      const fallbackErrorMsg = `Failed to parse JSON from a non-OK response. Status: ${response.status} ${response.statusText}. ${contextErrorMessage || ""}`;
-      console.error(fallbackErrorMsg, { headers: response.headers, url: response.url });
-      throw new Error(fallbackErrorMsg);
+      errorMsg = `Failed to parse JSON. ${errorMsg}`;
+      console.error(errorMsg, { headers: response.headers, url: response.url });
+      throw new Error(errorMsg);
     }
   }
 
@@ -22,7 +18,7 @@ export async function handleResponse(response: Response, contextErrorMessage?: s
     const data = await response.json();
     return data;
   } catch (error) {
-    const errorMsg = `Failed to parse JSON. Status: ${response.status} ${response.statusText}. ${contextErrorMessage || ""}`;
+    errorMsg = `Failed to parse JSON. ${errorMsg}`;
     console.error(errorMsg, { headers: response.headers, url: response.url });
     throw new Error(errorMsg);
   }
