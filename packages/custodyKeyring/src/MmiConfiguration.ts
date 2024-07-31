4a320bcb794c00e604958ab06fdcdbc7871bbe2d
Shane Terence Odlum <shane.odlum@consensys.net>
2024-07-31 10:38:16 +0100

    chore(custody-keyring): get the bitgo api url from configuration api

--- packages/custodyKeyring/src/MmiConfiguration.ts
+++ packages/custodyKeyring/src/MmiConfiguration.ts
@@ -11,6 +11,10 @@ import { ICustodianEnvironment } from "./interfaces/ICustodianEnvironment";
 import { IMmiConfigurationControllerOptions } from "./interfaces/IMmiConfigurationControllerOptions";
 import { MMIConfiguration } from "./types/MMIConfiguration";
 
+// If a custodian is `curv`, `qredo` `bitgo` or `cactus` it is legacy custodian
+
+const legacyCustodianNames = ["curv", "qredo", "bitgo", "cactus"];
+
 /**
  * Background controller responsible for maintaining
  * a cache of MMI Portfolio related data in local storage
@@ -64,6 +68,8 @@ export class MmiConfigurationController {
     const { mmiConfiguration } = this.store.getState(); // Stored configuration
     const configuredCustodians = configuration.custodians;
 
+    // Steo 1: populate the custodian array with the LEGACY custodians
+
     // Mutate custodians by adding information from the hardcoded types
     const custodians: ICustodianEnvironment[] = [
       ...Object.values(CUSTODIAN_TYPES)
@@ -89,10 +95,16 @@ export class MmiConfigurationController {
         })),
     ];
 
+    // Step 2: populate the custodian array with the configured custodians, which INCLUDES stubs for the legacy custodians
+    // We also mutate the legacy custodian, adding the isNoteToTraderSupported property (FIXME: this is no longer needed because there are no legacy custodians supporting this)
+
     // Loop through the custodians from the API
     configuredCustodians.forEach(custodian => {
       custodian.environments.forEach(environment => {
-        if (!environment.apiBaseUrl) {
+        // This used to check if there is no apiUrl, but now it checks by name
+        if (legacyCustodianNames.includes(environment.name)) {
+          // This logic checks if something is a legacy custodian
+
           // Version 1 custodians should still support note to trader
           // Find the legacy custodians in the list of custodians we are building
           const legacyCustodian = custodians.find(
@@ -103,7 +115,7 @@ export class MmiConfigurationController {
           } else {
             legacyCustodian.isNoteToTraderSupported = environment.isNoteToTraderSupported;
           }
-          return; // Use return instead of continue
+          return; // Exit this routine to avoid double adding the legacy custodian
         }
 
         custodians.push({
