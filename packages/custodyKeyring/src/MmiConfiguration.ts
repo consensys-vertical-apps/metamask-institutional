2e869d11e8a0796d7fb5e3a0cea99358c68545e7
Albert Olivé <albertolivecorbella@gmail.com>
2024-08-28 17:02:53 +0200

    fix: mmi xxx bitgo api url (#790)
    * chore(custody-keyring): get the bitgo api url from configuration api
    
    * fix(custody-keyrings): use config api isManualTokenInputSupported
    
    * chore(custodykeyring): improving logic
    
    * chore(custody-keyring): improve code
    
    * chore(github): remove support for node 16
    
    * fix(tests): fix tests
    
    * chore(property value): this default value should be false, since it only exists in dev config
    
    * refactor(mmi_configuration): checks for the api url first before assigning it
    
    * chore(configuration): updates config
    
    * fix(remove file): removes file
    
    ---------
    
    Co-authored-by: Shane Terence Odlum <shane.odlum@consensys.net>
    Co-authored-by: Antonio Regadas <antonio.regadas@consensys.net>

--- packages/custodyKeyring/src/MmiConfiguration.ts
+++ packages/custodyKeyring/src/MmiConfiguration.ts
@@ -11,6 +11,9 @@ import { ICustodianEnvironment } from "./interfaces/ICustodianEnvironment";
 import { IMmiConfigurationControllerOptions } from "./interfaces/IMmiConfigurationControllerOptions";
 import { MMIConfiguration } from "./types/MMIConfiguration";
 
+// If a custodian is `curv`, `qredo` `bitgo` or `cactus` it is legacy custodian
+const legacyCustodianNames = ["curv", "qredo", "bitgo", "cactus"];
+
 /**
  * Background controller responsible for maintaining
  * a cache of MMI Portfolio related data in local storage
@@ -29,18 +32,18 @@ export class MmiConfigurationController {
     const initState = opts.initState?.mmiConfiguration
       ? opts.initState
       : {
-          mmiConfiguration: {
-            portfolio: {
-              enabled: false,
-              url: "",
-              cookieSetUrls: [],
-            },
-            features: {
-              websocketApi: false,
-            },
-            custodians: [], // NB: Custodians will always be empty when we start
+        mmiConfiguration: {
+          portfolio: {
+            enabled: false,
+            url: "",
+            cookieSetUrls: [],
           },
-        };
+          features: {
+            websocketApi: false,
+          },
+          custodians: [], // NB: Custodians will always be empty when we start
+        },
+      };
 
     this.configurationClient = new ConfigurationClient(opts.mmiConfigurationServiceUrl);
 
@@ -64,6 +67,7 @@ export class MmiConfigurationController {
     const { mmiConfiguration } = this.store.getState(); // Stored configuration
     const configuredCustodians = configuration.custodians;
 
+    // Steo 1: populate the custodian array with the LEGACY custodians
     // Mutate custodians by adding information from the hardcoded types
     const custodians: ICustodianEnvironment[] = [
       ...Object.values(CUSTODIAN_TYPES)
@@ -89,11 +93,16 @@ export class MmiConfigurationController {
         })),
     ];
 
+    // Step 2: populate the custodians array with the configured custodians, which already INCLUDES the legacy custodians
+    // We also mutate the legacy custodian, adding the API url when it exists on the configured custodians side (Bitgo dev/test env)
+
     // Loop through the custodians from the API
     configuredCustodians.forEach(custodian => {
       custodian.environments.forEach(environment => {
-        if (!environment.apiBaseUrl) {
-          // Version 1 custodians should still support note to trader
+        // This used to check if there is no apiUrl, but now it checks by name
+        if (legacyCustodianNames.includes(environment.name)) {
+          // This logic checks if something is a legacy custodian
+
           // Find the legacy custodians in the list of custodians we are building
           const legacyCustodian = custodians.find(
             legacyCustodian => legacyCustodian.name.toLowerCase() === environment.name,
@@ -101,9 +110,13 @@ export class MmiConfigurationController {
           if (!legacyCustodian) {
             console.warn(`Missing legacy custodian ${environment.name}`);
           } else {
-            legacyCustodian.isNoteToTraderSupported = environment.isNoteToTraderSupported;
+            if(environment.apiBaseUrl) {
+              // Updates a legacy custodian with an API url from the configured custodian,
+              // it's useful for bitgo that has a different API url from dev to prod
+              legacyCustodian.apiUrl = environment.apiBaseUrl;
+            }
           }
-          return; // Use return instead of continue
+          return; // Exit this routine to avoid double adding the legacy custodian
         }
 
         custodians.push({
